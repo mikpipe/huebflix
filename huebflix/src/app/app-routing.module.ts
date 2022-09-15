1abda9d6e832417d8c5c1994d7ebf0c7e3b7f986
26918@students.hsrw <jonas-josef.holm@hsrw.org>
2022-09-16 00:48:54 +0200

    Routing

--- huebflix/src/app/app-routing.module.ts
+++ huebflix/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingpageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
